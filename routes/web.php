ee8b4e46fb950cb4d71cda1824ed39e23fc56eb5
parshad11 <52694914+parshad11@users.noreply.github.com>
2025-02-17 16:59:12 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Route::get('admin',[RoutingController::class,'adminpage']);
 Route::get('message',[RoutingController::class,'messagepage']);
 Route::post('insert',[MessagesController::class,'insert_data']);
-Route::post('insert-admin-message',[MessagesController::class,'insert_admin_data']);
+Route::post('insert-admin-message',[MessagesController::class,'insert_admin_test_test']);
 Route::put('update-admin-message',[MessagesController::class,'insert_admin_data']);
 
 
